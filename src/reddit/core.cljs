cde8ef4985c255a5d5b8354e380f875eade9e2c1
Xavier E. López <xavierelopez@gmail.com>
2014-06-23 10:31:06 -0400

    use a single channel for child header event communication

--- src/reddit/core.cljs
+++ src/reddit/core.cljs
@@ -20,8 +20,9 @@
   (reddit/get-subreddit-posts (:subreddit @app) (:selected-filter @app)))
 
 (defcomponent subreddit-list [subs]
-  (render-state [_ {:keys [select-channel]}]
-    (let [build-li (fn [sub] [:li {:on-click #(put! select-channel (.. % -target -innerText))} sub])]
+  (render-state [_ {:keys [events]}]
+    (let [build-li (fn [sub] [:li {
+        :on-click #(put! events {:name :subreddit-selected, :value  (.. % -target -innerText)})} sub])]
       (html [:ul {:class "subreddits"} (map build-li subs)]))))
 
 (defcomponent post-list [posts]
@@ -30,35 +31,33 @@
       (map (fn [p] [:li [:strong (:title p)] [:em (str " by " (:author p))]]) posts)])))
 
 (defcomponent select-box [{:keys [values selected]} owner]
-  (render-state [_ {:keys [select-channel]}]
+  (render-state [_ {:keys [events]}]
     (html [:select {:value selected
-                    :on-change #(put! select-channel (.. % -target -value))}
+                    :on-change #(put! events {:name :filter-selected, :value (.. % -target -value)})}
       (map (fn [value] [:option value]) values)])))
 
 (defcomponent header [app owner]
   (init-state [_]
-    {:sub-select-ch (chan)
-     :filter-select-ch (chan)})
+    {:events (chan)})
   (will-mount [_]
-    (let [sub-select-ch (om/get-state owner :sub-select-ch)
-          filter-select-ch (om/get-state owner :filter-select-ch)]
+    (let [events (om/get-state owner :events)]
       (go (loop []
-        (let [[value channel] (alts! [sub-select-ch filter-select-ch])]
-          (condp = channel
-            sub-select-ch (om/update! app :subreddit value)
-            filter-select-ch (om/update! app :selected-filter value))
+        (let [event (<! events)]
+          (condp = (:name event)
+            :subreddit-selected (om/update! app :subreddit (:value event))
+            :filter-selected (om/update! app :selected-filter (:value event)))
           (om/update! app :posts [])
           (om/update! app :posts (<! (fetch-posts)))) (recur)))))
-  (render-state [_ {:keys [sub-select-ch filter-select-ch]}]
+  (render-state [_ {:keys [events]}]
     (html [:div {:id "header"}
       [:h1 "reddit cmn"]
       [:h2 (str "r/" (:subreddit app))]
       [:h2 (str "Filter: " (:selected-filter app))]
       (om/build subreddit-list (:subreddits app)
-                               {:init-state {:select-channel sub-select-ch}})
+                               {:init-state {:events events}})
       (om/build select-box {:values (:filters app)
                             :selected (:selected-filter app)}
-                            {:init-state {:select-channel filter-select-ch}})])))
+                            {:init-state {:events events}})])))
 
 (defcomponent main [app owner]
   (will-mount [_]
