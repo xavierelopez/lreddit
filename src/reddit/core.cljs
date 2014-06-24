7460f2c21b57e0e4bfad299f76ccaf6b50378a8e
Xavier E. López <xavierelopez@gmail.com>
2014-06-23 20:07:39 -0400

    add root component in charge of routing views

--- src/reddit/core.cljs
+++ src/reddit/core.cljs
@@ -1,6 +1,8 @@
 (ns reddit.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
+            [om-tools.core :refer-macros [defcomponent]]
+            [cljs.core.async :refer [put! chan <! alts! timeout]]
             [reddit.components.main :refer [main]]))
 
 (enable-console-print!)
@@ -8,13 +10,23 @@
 (def app (atom {:comment ""
                 :post-id nil
                 :posts []
+                :view :main
                 :subreddit "askreddit"
                 :subreddits ["askreddit" "asksciencefiction" "truereddit" "iama"]
                 :filters ["hot" "new" "top"]
                 :filter-times ["week" "month" "year" "all"]
                 :selected-filter {:name "hot", :time "week"}}))
 
+
+(defcomponent root [app owner]
+  (render [_]
+    (let [view (:view app)]
+      (om/build
+       (condp = view
+         :main main
+         (om/div nil "Empty")) app))))
+
 (om/root
-  main
+  root
   app
   {:target (. js/document (getElementById "app"))})
