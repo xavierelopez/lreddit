bfc053249fd127e814ace0e07f9081ff4b011aab
Xavier E. López <xavierelopez@gmail.com>
2014-06-24 00:42:18 -0400

    nil start

--- src/reddit/core.cljs
+++ src/reddit/core.cljs
@@ -17,7 +17,7 @@
 (def app (atom {:comment ""
                 :post-id nil
                 :posts []
-                :view :main
+                :view nil
                 :subreddit "askreddit"
                 :subreddits ["askreddit" "asksciencefiction" "truereddit" "iama"]
                 :filters ["hot" "new" "top"]
