c709998c2c225068992d5f443efbc0338c503899
Xavier E. López <xavierelopez@gmail.com>
2014-06-23 20:24:17 -0400

    add basic secretary router

--- src/reddit/core.cljs
+++ src/reddit/core.cljs
@@ -3,28 +3,42 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
             [cljs.core.async :refer [put! chan <! alts! timeout]]
-            [reddit.components.main :refer [main]]))
+            [secretary.core :as secretary :include-macros true :refer [defroute]]
+            [reddit.components.main :refer [main]]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType])
+  (:import goog.History))
 
 (enable-console-print!)
 
 (def app (atom {:comment ""
                 :post-id nil
                 :posts []
-                :view :main
+                :view nil
                 :subreddit "askreddit"
                 :subreddits ["askreddit" "asksciencefiction" "truereddit" "iama"]
                 :filters ["hot" "new" "top"]
                 :filter-times ["week" "month" "year" "all"]
                 :selected-filter {:name "hot", :time "week"}}))
 
+; Router setup
+
+(secretary/set-config! :prefix "#")
+
+(defroute "/" [] (swap! app assoc :view :main))
+
+(let [h (History.)]
+  (goog.events/listen h EventType/NAVIGATE #(secretary/dispatch! (.-token %)))
+  (doto h (.setEnabled true)))
+
+; End router setup
 
 (defcomponent root [app owner]
   (render [_]
     (let [view (:view app)]
       (om/build
        (condp = view
-         :main main
-         (om/div nil "Empty")) app))))
+         :main main) app))))
 
 (om/root
   root
