f95f305825ab54de0c4de678baaf2999b47c5372
Xavier E. López <xavierelopez@gmail.com>
2014-06-29 12:42:49 -0400

    add Loading… text to post page

--- src/cljs/reddit/components/post.cljs
+++ src/cljs/reddit/components/post.cljs
@@ -40,9 +40,10 @@
     (let [{:keys [author title selftext_html]} (get-in app [:post :parent])
           replies (get-in app [:post :replies])]
       (html [:div {:id "post"}
-        [:div {:class "main-parent"}
-          [:h3 {:class "post-title"} title]
-          [:span {:class "post-author"} (str "by " author)]
-          [:p {:class "post-body" :dangerouslySetInnerHTML {:__html (unescape-html selftext_html)}}]]
-        [:ul {:class "replies"} (om/build-all reply-view replies)]]))))
+        (if (empty? author) [:span "Loading..."]
+          [:div {:class "main-parent"}
+            [:h3 {:class "post-title"} title]
+            [:span {:class "post-author"} (str "by " author)]
+            [:p {:class "post-body" :dangerouslySetInnerHTML {:__html (unescape-html selftext_html)}}]]
+          [:ul {:class "replies"} (om/build-all reply-view replies)])]))))
 
